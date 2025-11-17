77b652db9af22a92dfb9876b68367d93181f1a15
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-17 18:17:56 +0300

    Remove echo from to git or not to git

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,13 +1,3 @@
 #/bin/bash
 
-# Fetch superheroes & use jq to access item whose id is 170
-obj_170=$(curl -s "https://learn.zone01kisumu.ke/assets/superhero/all.json" | jq '.[] | select(.id == 170)')
-
-# Added -r flag to ensure output is plain strings ie lack ""
-name=$(echo "$obj_170" | jq -r '.name')
-power=$(echo "$obj_170" | jq -r '.powerstats.power')
-gender=$(echo "$obj_170" | jq -r '.appearance.gender')
-
-echo ${name}
-echo ${power}
-echo ${gender}
+curl -s "https://learn.zone01kisumu.ke/assets/superhero/all.json" | jq -r '.[] | select(.id == 170) | .name, .powerstats.power, .appearance.gender'
