eafa8d12bcfc042bc85c1f271f0dad8bfe8d5889
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-18 13:05:49 +0300

    Ensure newlines get removed

--- myfamily.sh
+++ myfamily.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
-curl -s "https://learn.zone01kisumu.ke/assets/superhero/all.json" | jq -r ".[] | select(.id == ${HERO_ID}) | .connections.relatives"
+curl -s "https://learn.zone01kisumu.ke/assets/superhero/all.json" | 
+  jq -r ".[] | select(.id == ${HERO_ID}) | .connections.relatives" | 
+  tr '\n' ' ' | sed 's/ $//'
