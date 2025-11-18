5ab1137a30f3bb98a0dbe2656b6fbcbfdcd78eac
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-18 15:32:03 +0300

    Use set & pattern matching to ensure folder hierachy gets removed & we remain with the only the file

--- lookagain.sh
+++ lookagain.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
 
 # "sort -r" -> r flag to reverse the default ascending order
-# "cut -c 3-" -> -c flag to specify character count that I want
-#             -> "3-" specify to take charactes starting from index 3 to the end ie exclude "./" 
+# 'sed "s/.*\///"' -> substitute charachters from "." to '/' with nothing ie remove parent directories
 # 'sed "s/.sh//g"' -> Replace all instances of ".sh" with nothing ie specified by //
-find . -name "*.sh" | sort -r | cut -c 3- | sed "s/.sh//g"
+find . -name "*.sh" | sed 's/.*\///' | sed "s/.sh//g" | sort -r 
