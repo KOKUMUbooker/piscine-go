f44d08e34653d7ccd7d9f74e4aa8dd209a35d4e0
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-18 14:38:32 +0300

    Ensure items are in descending order

--- lookagain.sh
+++ lookagain.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
-find . -type f -name "*.sh" | cut -c 3- | sed "s/.sh//g"
+# "sort -r" -> r flag to reverse the default ascending order
+# "cut -c 3-" -> -c flag to specify character count that I want
+#             -> "3-" specify to take charactes starting from index 3 to the end ie exclude "./" 
+# 'sed "s/.sh//g"' -> Replace all instances of ".sh" with nothing ie specified by //
+find . -name "*.sh" | sort -r | cut -c 1- | sed "s/.sh//g"
