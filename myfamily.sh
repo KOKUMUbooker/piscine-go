22475be0c92e011d441df4aa92de476c078a0035
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-18 13:33:31 +0300

    Redo myFamily.sh by passing variables into the jq query using --argjson & ensuring all " get removed using sed instead of passing -r option to jq

--- myfamily.sh
+++ myfamily.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
-curl -s "https://learn.zone01kisumu.ke/assets/superhero/all.json" | 
-  jq -r ".[] | select(.id == ${HERO_ID}) | .connections.relatives" | 
-  tr '\n' ' ' | sed 's/ $//'
+# --argjson hId "$HERO_ID" -> safely pass env var HERO_ID under a new variable name hId for access under the jq query
+# seg 's/"//g' -
+#   s -> substitute, the action
+#   " -> pattern to substitute
+#   // -> what to be used for substituting ie nothing
+#   g -> Ensures action is executed for every occurence of the pattern
+
+curl -s "https://learn.zone01kisumu.ke/assets/superhero/all.json" | jq --argjson hId "$HERO_ID" '.[] | select(.id == $hId) | .connections.relatives' | sed 's/"//g'
