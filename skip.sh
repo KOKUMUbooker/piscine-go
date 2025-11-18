f5d37efdd0fb19cafaf08bc4f0cc35199d262302
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-18 17:01:13 +0300

    Complete skip.sh

---
+++ skip.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# NF -> inbuilt variable that points to the last column
+# awk '{print $NF}' -> print last column
+# sed '1~2c\' -> replace odd number lines with nothing ie delete them
+#             -> 1~2 specifies the range
+#                where 1 is the starting point
+#                ~2 specifies number of steps to be incremented till the end is reached
+#                eg 1, 3(1+2), 5(3+2), 7(5+2)
+#              -> c - to specify action is change
+#              -> \ - specify the action to be used to achieve the action ie
+#                 change all odd numbered line with nothing ie delete them
+ls -l | awk '{print $NF}' | sed '1~2c\'
