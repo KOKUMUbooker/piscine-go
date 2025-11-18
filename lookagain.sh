50476d717c0ce5b933c9a35d58aec4dff20ecd21
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-18 14:42:51 +0300

    Ensure cut starting point is 3

--- lookagain.sh
+++ lookagain.sh
@@ -4,4 +4,4 @@
 # "cut -c 3-" -> -c flag to specify character count that I want
 #             -> "3-" specify to take charactes starting from index 3 to the end ie exclude "./" 
 # 'sed "s/.sh//g"' -> Replace all instances of ".sh" with nothing ie specified by //
-find . -name "*.sh" | sort -r | cut -c 1- | sed "s/.sh//g"
+find . -name "*.sh" | sort -r | cut -c 3- | sed "s/.sh//g"
