2f095212af895b9cd9ec2e723337c3c50e447eb3
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-19 15:23:59 +0300

    complete printalphabet

---
+++ printalphabet/go.mod
@@ -0,0 +1,5 @@
+module printalphabet
+
+go 1.24.3
+
+require github.com/01-edu/z01 v0.2.0
---
+++ printalphabet/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func main(){
+	// In ascii table lower case letters start from 97 to 122	
+	for i := 97; i <= 122; i++ {
+		z01.PrintRune(rune(i));
+	}
+}
