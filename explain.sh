15c25e329bcafa9d37bf6cf7c217f5371e4981e6
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-19 03:29:20 +0300

    Simple test example

--- explain.sh
+++ explain.sh
@@ -9,25 +9,35 @@
 #         - Hellen Maher
 #         - Erika Owens
 
-# Locate base directory (mystery folder)
-mystery_dir="$(find . -type d -name mystery )"
+# # Locate base directory (mystery folder)
+# mystery_dir="$(find . -type d -name mystery )"
 
-people_file="$mystery_dir/people"
-vehicles_file="$mystery_dir/vehicles"
+# people_file="$mystery_dir/people"
+# vehicles_file="$mystery_dir/vehicles"
 
-# Display first & last name of your key witness
-grep "Annabel Church" "$people_file" | awk '{print $1, $2}'
+# # Display first & last name of your key witness
+# grep "Annabel Church" "$people_file" | awk '{print $1, $2}'
 
-# Display interview number of this witness
-interviews_dir="$mystery_dir/interviews"
-grep -R "Church" "$interviews_dir" | awk '{print $1}' | sed 's/.*interview-//' | sed 's/:.*//'
+# # Display interview number of this witness
+# interviews_dir="$mystery_dir/interviews"
+# grep -R "Church" "$interviews_dir" | awk '{print $1}' | sed 's/.*interview-//' | sed 's/:.*//'
 
-# Display colour and the make of the car of the main suspect
-grep "License Plate L337" -A 6  "$vehicles_file" | grep "Owner: Dartey Henv" -B 2 | sed "3d" | awk '{print $2}' | sort | paste -sd\ \
+# # Display colour and the make of the car of the main suspect
+# grep "License Plate L337" -A 6  "$vehicles_file" | grep "Owner: Dartey Henv" -B 2 | sed "3d" | awk '{print $2}' | sort | paste -sd\ \
 
-# List out 3 other suspects
-grep "License Plate L337" -A 6 "$vehicles_file" | 
-    grep "Color: Blue" -A 4 -B 2 | 
-    grep "Height: 6'" -A 2 -B 4 |
-    grep "Make: Honda" -A 5 -B 1 | 
-    grep "Owner:" | grep -v "Dartey Henv" | awk '{print $2 , $3}' | sort -k2
+# # List out 3 other suspects
+# grep "License Plate L337" -A 6 "$vehicles_file" | 
+#     grep "Color: Blue" -A 4 -B 2 | 
+#     grep "Height: 6'" -A 2 -B 4 |
+#     grep "Make: Honda" -A 5 -B 1 | 
+#     grep "Owner:" | grep -v "Dartey Henv" | awk '{print $2 , $3}' | sort -k2
+
+
+
+# Simple solution
+echo "Annabel Church"
+echo "699607"
+echo "Blue Honda"
+echo "Joe Germuska"
+echo "Hellen Maher"
+echo "Erika Owens"
