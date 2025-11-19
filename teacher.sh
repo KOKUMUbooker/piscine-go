bfad15e7d456cd28f58c2e6751547b525f424196
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-19 13:21:38 +0300

    Modifications

--- teacher.sh
+++ teacher.sh
@@ -3,14 +3,11 @@
 # Get interview number of this witness
 iNum=$(grep "Church" interviews/* | awk '{print $1}' | sed 's/.*interview-//' | sed 's/:.*//')
 
-# Isolate interview number into an env variable
-export interviewnum=$iNum
-
 # Print out env variable
-echo $interviewnum
+echo $iNum
 
 # Print out the contents f the interview
-cat "interviews/interview-$iNum"
+cat "interviews/interview-${iNum}"
 
 # Print out contents of main suspect
 echo "$MAIN_SUSPECT"
