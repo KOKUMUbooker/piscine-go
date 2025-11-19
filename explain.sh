b23f77668ae8ef42a826b394da1e0d7e216ccc02
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-19 03:23:42 +0300

    Remove hardcoded path directory & look for starting based dir - mystery dir

--- explain.sh
+++ explain.sh
@@ -9,17 +9,24 @@
 #         - Hellen Maher
 #         - Erika Owens
 
+# Locate base directory (mystery folder)
+mystery_dir="$(find . -type d -name mystery )"
+
+people_file="$mystery_dir/people"
+vehicles_file="$mystery_dir/vehicles"
+
 # Display first & last name of your key witness
-grep "Annabel Church" people | awk '{print $1, $2}'
+grep "Annabel Church" "$people_file" | awk '{print $1, $2}'
 
 # Display interview number of this witness
-grep "Church" interviews/* | awk '{print $1}' | sed 's/.*interview-//' | sed 's/:.*//'
+interviews_dir="$mystery_dir/interviews"
+grep -R "Church" "$interviews_dir" | awk '{print $1}' | sed 's/.*interview-//' | sed 's/:.*//'
 
 # Display colour and the make of the car of the main suspect
-grep "License Plate L337" -A 6  vehicles | grep "Owner: Dartey Henv" -B 2 | sed "3d" | awk '{print $2}' | sort | paste -sd\ \
+grep "License Plate L337" -A 6  "$vehicles_file" | grep "Owner: Dartey Henv" -B 2 | sed "3d" | awk '{print $2}' | sort | paste -sd\ \
 
 # List out 3 other suspects
-grep "License Plate L337" -A 6 vehicles | 
+grep "License Plate L337" -A 6 "$vehicles_file" | 
     grep "Color: Blue" -A 4 -B 2 | 
     grep "Height: 6'" -A 2 -B 4 |
     grep "Make: Honda" -A 5 -B 1 | 
