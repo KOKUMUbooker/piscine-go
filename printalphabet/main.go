d13c124b217fd2b2741cf290b00b97e787ecdbe0
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-19 15:25:33 +0300

    Format file

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -4,9 +4,9 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func main(){
-	// In ascii table lower case letters start from 97 to 122	
+func main() {
+	// In ascii table lower case letters start from 97 to 122
 	for i := 97; i <= 122; i++ {
-		z01.PrintRune(rune(i));
+		z01.PrintRune(rune(i))
 	}
 }
