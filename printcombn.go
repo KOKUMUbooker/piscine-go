caad1223deacb178049f256e6c7c55c23e6a427c
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-20 14:31:08 +0300

    Optimization change

--- printcombn.go
+++ printcombn.go
@@ -2,11 +2,14 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func PrintComb3() {
+func PrintCombUnoptimized() {
 	isFirst := true
 
+	// Loop 1: First digit (i) - Iterates 10 times ('0' to '9')
 	for i := '0'; i <= '9'; i++ {
+		// Loop 2: Second digit (j) - Iterates 10 times ('0' to '9')
 		for j := '0'; j <= '9'; j++ {
+			// Loop 3: Third digit (k) - Iterates 10 times ('0' to '9')
 			for k := '0'; k <= '9'; k++ {
 				if i < j && j < k {
 
