3c2dc8097fa922c3bcd5582ad438711c827782e5
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-20 05:50:35 +0300

    Fix looping approach

--- printcomb2.go
+++ printcomb2.go
@@ -7,14 +7,28 @@ func PrintComb2() {
 
 	for i := '0'; i <= '9'; i++ {
 		for j := '0'; j <= '9'; j++ {
-			if !first {
-				z01.PrintRune(',')
-				z01.PrintRune(' ')
-			}
+			for k := '0'; k <= '9'; k++ {
+				for l := '0'; l <= '9'; l++ {
+					// If ij similar to kl skip iteration
+					if (i == k && j == l) {
+						continue;
+					}
 
-			first = false
-			z01.PrintRune(i)
-			z01.PrintRune(j)
+					if !first { // Only add ", " if not in first set of iteration
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
+		
+					first = false;
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					
+					z01.PrintRune(' ')
+
+					z01.PrintRune(k)
+					z01.PrintRune(l)
+				}
+			}
 		}
 	}
 
