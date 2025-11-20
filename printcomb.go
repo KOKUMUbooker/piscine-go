2ad0a6f7f7e96a22310fb7eb8a6373f637e08f7e
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-20 05:21:18 +0300

    Correctly format printcomb.go

--- printcomb.go
+++ printcomb.go
@@ -2,32 +2,30 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-
 func PrintComb() {
-  first := false;
+	first := false
 
-  for i := '0'; i <= '9'; i++ {
-    for j := '0'; j <= '9'; j++ {
-      for k := '0'; k <= '9'; k++ {
+	for i := '0'; i <= '9'; i++ {
+		for j := '0'; j <= '9'; j++ {
+			for k := '0'; k <= '9'; k++ {
 
-        if i == j && j == k {
-          continue;
-        }
+				if i == j && j == k {
+					continue
+				}
 
-        if i < j && j < k {
-          if first { // First time don't prepend ", ", but subsequent cycles prepend it
-            z01.PrintRune(',')
-            z01.PrintRune(' ')
-          }
+				if i < j && j < k {
+					if first { // First time don't prepend ", ", but subsequent cycles prepend it
+						z01.PrintRune(',')
+						z01.PrintRune(' ')
+					}
 
-          first = true
-          z01.PrintRune(i);
-          z01.PrintRune(j);
-          z01.PrintRune(k);
-        }
-      }
-    }
-  }
-  z01.PrintRune('\n')
+					first = true
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(k)
+				}
+			}
+		}
+	}
+	z01.PrintRune('\n')
 }
-
