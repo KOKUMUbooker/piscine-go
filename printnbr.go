ab656867f1f4dc7aaca8038d102e1cc2ae743dd6
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-20 11:58:06 +0300

    Properly handle overflow issues

--- printnbr.go
+++ printnbr.go
@@ -4,27 +4,57 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func check(r int) {
-	c := '0'
-	if r == 0 {
-		z01.PrintRune(c)
-		return
-	}
-	for i := 1; i <= r%10; i++ {
-		c++
-	}
-	for i := -1; i >= r%10; i-- {
-		c++
-	}
-	if r/10 != 0 {
-		check(r / 10)
-	}
-	z01.PrintRune(c)
-}
+/*
+	1. Recieve int param
+	2. Break int into individual digits
+	3. Convert individual digits into rune
+	4. Iteratively call PrinRune() on the array of runes
+*/
 
 func PrintNbr(n int) {
+	splitDigits := splitIntToDigits(n) // Digits will be in reverse
+	splitDigitsRunes := make([]rune, 0)
+
 	if n < 0 {
-		z01.PrintRune('-')
+		splitDigitsRunes = append(splitDigitsRunes, '-') // Prepend '-' for negative numbers
 	}
-	check(n)
+
+	for i := len(splitDigits) - 1; i >= 0; i-- {
+		r := rune('0' + splitDigits[i])
+		splitDigitsRunes = append(splitDigitsRunes, r)
+	}
+
+	for _, r := range splitDigitsRunes {
+		z01.PrintRune(r)
+	}
+}
+
+/*
+Splitting int to digits is achieved using 2 expressions - division by 10 & modulus by 10
+  - These 2 operation are repeated until the quotient of the division expression is 0
+  - Modulus expression gives the split digit under that iteration
+*/
+func splitIntToDigits(n int) []int {
+	digits := make([]int, 0)
+	quot := n
+	mod := 0
+
+	if n == 0 {
+		return []int{0}
+	}
+
+	// Do this until quotient is zero
+	for quot != 0 {
+		// Handle potential overflow issues
+		if n < 0 { // if negative calculate modulus as it
+			mod = (quot % 10)
+		} else { // if positive negate result since, negative int values are more that positive int values
+			mod = -(quot % 10)
+		}
+
+		digits = append(digits, -mod)
+		quot = quot / 10
+	}
+
+	return digits
 }
