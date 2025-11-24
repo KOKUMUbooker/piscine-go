bec14e0b6793f78514cdd53aa9eff8275c1b6424
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-24 09:49:52 +0300

    Add overflow check

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -3,9 +3,11 @@ package piscine
 // Factorial - product of all positive ints less that or equal to a number
 // eg 5! = 5 * 4 * 3 * 2 *1
 func IterativeFactorial(nb int) int {
-	if nb <= 0 {
+	// Factorial of 25 results in a int overflow on the positive side
+	if nb <= 0 || nb >= 25 {
 		return 0
 	}
+
 	if nb == 1 {
 		return 1
 	}
