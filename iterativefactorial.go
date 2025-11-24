a980237035d15169fbbdff0c98299496049361ad
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-24 09:44:07 +0300

    Minor change

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -3,8 +3,11 @@ package piscine
 // Factorial - product of all positive ints less that or equal to a number
 // eg 5! = 5 * 4 * 3 * 2 *1
 func IterativeFactorial(nb int) int {
-	if nb < 0 {
+	if nb <= 0 {
 		return 0
+	}
+	if nb == 1 {
+		return 1
 	}
 
 	result := 1
