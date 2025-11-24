cb4601a95750955883484f8e53de4a1dee4297e6
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-24 09:33:39 +0300

    Complete iterative fact

---
+++ iterativefactorial.go
@@ -0,0 +1,16 @@
+package piscine
+
+// Factorial - product of all positive ints less that or equal to a number
+// eg 5! = 5 * 4 * 3 * 2 *1
+func IterativeFactorial(nb int) int {
+	if nb < 0 {
+		return 0
+	}
+
+	result := 1
+	for i := 1; i <= nb; i++ {
+		result = result * i
+	}
+
+	return result
+}
