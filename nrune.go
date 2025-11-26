9df602c66035b4cf8a94adf6a8701e1877c0bc1f
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-26 06:43:58 +0300

    Update early return checks

--- nrune.go
+++ nrune.go
@@ -1,13 +1,14 @@
 package piscine
 
+// NRune("Whatshisname", -2) == 'm' instead of '\x00'
 // piscine.NRune("Ola!", 4)
 func NRune(s string, n int) rune {
-	if n > len(s) {
+	if n > len(s) || n < -1 {
 		return 0
 	}
 
 	iVal := n - 1 // eg n = 2 should mean 2nd letter ie i = 1
-	if n < 0 {
+	if n == -1 {
 		iVal = len(s) + n
 	}
 
