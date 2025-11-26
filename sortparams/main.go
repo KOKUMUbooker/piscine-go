3959c4696af91b5a5046628381cee0536d872179
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-26 20:51:38 +0300

    Accomodate strings not just runes

--- sortparams/main.go
+++ sortparams/main.go
@@ -7,29 +7,69 @@ import (
 )
 
 func main() {
-	paramsR := []rune{}
+	params := []string{}
 
 	if len(os.Args) > 1 {
 		for _, str := range os.Args[1:] {
 			if len(str) > 0 {
-				paramsR = append(paramsR, rune(str[0]))
+				params = append(params, str)
 			}
 		}
 	}
 
-	bubbleSort(paramsR)
+	bubbleSort(params)
 
-	for _, r := range paramsR {
-		z01.PrintRune(r)
+	for _, str := range params {
+		for _, r := range str {
+			z01.PrintRune(r)
+		}
 		z01.PrintRune('\n')
 	}
 }
 
-func bubbleSort(rSlice []rune) {
+// Returns true if a > b
+func isGreater(a, b string) bool {
+	res := true
+
+	if len(a) == 1 && len(b) == 1 {
+		res = rune(a[0]) > rune(b[0])
+	} else if len(a) == len(b) {
+		for i, ar := range a {
+			br := rune(b[i])
+			if ar != br {
+				res = ar > br
+				break
+			}
+		}
+	} else if len(a) > len(b) {
+		for i, br := range b {
+			ar := rune(b[i])
+			if ar != br {
+				res = ar > br
+				break
+			}
+		}
+	} else if len(a) < len(b) {
+		for i, ar := range a {
+			br := rune(b[i])
+			if ar != br {
+				res = ar > br
+				break
+			}
+			if res {
+				res = false
+			}
+		}
+	}
+
+	return res
+}
+
+func bubbleSort(rSlice []string) {
 	n := len(rSlice)
 	for i := 0; i < n-1; i++ {
 		for j := 0; j < n-1; j++ {
-			if rSlice[j] > rSlice[j+1] {
+			if isGreater(rSlice[j], rSlice[j+1]) {
 				rSlice[j], rSlice[j+1] = rSlice[j+1], rSlice[j]
 			}
 		}
