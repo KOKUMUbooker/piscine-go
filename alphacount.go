75946d7ed70ff70d59c82bff26a24a97f268c2c9
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-26 09:08:44 +0300

    Fix boolean exp issue

--- alphacount.go
+++ alphacount.go
@@ -3,7 +3,7 @@ package piscine
 func AlphaCount(s string) int {
 	count := 0
 	for _, r := range s {
-		if (r >= 'a' && r >= 'z') || (r >= 'A' && r >= 'Z') {
+		if (r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') {
 			count++
 		}
 	}
