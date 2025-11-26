49cd9bbef0f2301fdb36c053361c1e96e96ed7d2
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-26 06:14:19 +0300

    Complete nrune

---
+++ nrune.go
@@ -0,0 +1,22 @@
+package piscine
+
+func NRune(s string, n int) rune {
+	if n > len(s) - 1 {
+		return 0
+	}
+
+	iVal := n
+	if n < 0 {
+		iVal = len(s) + n
+	}
+
+	var runeVal rune
+	for i,r := range s {
+		if i == iVal {
+			runeVal = r
+			break
+		}
+	} 
+
+	return  runeVal
+}
