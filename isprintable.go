fd502483bc6aa7a752e401f407faf1f7e5054cb4
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-26 12:15:55 +0300

    Done isPrintableRune

--- isprintable.go
+++ isprintable.go
@@ -3,7 +3,7 @@ package piscine
 func IsPrintable(s string) bool {
 	isPrintable := true
 	for _, r := range s {
-		isPrintableRune := (r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') || (r >= '0' && r <= '9')
+		isPrintableRune := r >= '\u0020' && r <= '\u007f'
 		if !isPrintableRune {
 			isPrintable = false
 			break
