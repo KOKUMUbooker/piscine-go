52a554de28ce6dad1eff243ff8b96e6522f8096a
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-26 19:27:04 +0300

    Complete printprogramname

---
+++ printprogramname/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	progName := os.Args[0]
+	for _,r := range progName[2:] {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
