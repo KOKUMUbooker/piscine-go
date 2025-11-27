22f17c39389580e38c70a3bd71fb32ba8c3a41ed
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-27 14:21:25 +0300

    Complete appendrange

---
+++ appendrange.go
@@ -0,0 +1,16 @@
+package piscine
+
+func AppendRange(min, max int) []int {
+	var res []int
+
+	if min >= max {
+		return res
+	}
+
+	res = []int{}
+	for i := min; i <= max; i++ {
+		res = append(res, i)
+	}
+
+	return res
+}
