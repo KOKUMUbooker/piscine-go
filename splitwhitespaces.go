79eec579ba043c981d0d0d589fb9246f2d53202e
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-27 16:26:44 +0300

    complete split white spaces

---
+++ splitwhitespaces.go
@@ -0,0 +1,37 @@
+package piscine
+
+func SplitWhiteSpaces(s string) []string {
+	sR := []rune(s)
+	sRLen := len(sR)
+	strSlice := []string{}
+
+	i := 0
+	end := 0
+
+	for i < sRLen {
+		if sR[i] == ' ' {
+			i++
+			end++
+			continue
+		}
+
+		for sR[end] != ' ' {
+			end++
+			if end == sRLen {
+				break
+			}
+		}
+
+		word := ""
+		if end == sRLen {
+			word = string(sR[i : end-1])
+		} else {
+			word = string(sR[i:end])
+		}
+
+		strSlice = append(strSlice, word)
+		i = end
+	}
+
+	return strSlice
+}
