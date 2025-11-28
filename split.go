5625bab0db911971729ffc06531f44f82bab9187
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-28 17:12:26 +0300

    Complete split

--- split.go
+++ split.go
@@ -6,14 +6,45 @@ func Split(s, sep string) []string {
 	sepR := []rune(sep)
 
 	sRLen := len(sR)
-	sepLen := len(sepR)
+	spLen := len(sepR)
+
+	if len(sep) == 0 {
+		return []string{s}
+	}
 
 	i := 0
 	end := 0
 
 	for i < sRLen {
-		eOfSep := (i + sepLen) - 1
+		r := sR[i]
+		pendLen := len(sR[i:])
+		pendHoldsSep := pendLen >= len(sep)
+
+		if r == sepR[0] && pendHoldsSep && string(sR[i:i+spLen]) == sep {
+			i += spLen
+			end += spLen
+			continue
+		}
+
+		for {
+			if end >= sRLen {
+				break
+			}
+
+			eR := sR[end]
+			ePendLen := len(sR[end:])
+			ePendHoldsSep := ePendLen >= len(sep)
+
+			if eR == sepR[0] && ePendHoldsSep && string(sR[end:end+spLen]) == sep {
+				break
+			}
+			end++
+		}
+
+		word := string(sR[i:end])
+		sepStr = append(sepStr, word)
+		i = end
 	}
 
-	return  sepStr
+	return sepStr
 }
