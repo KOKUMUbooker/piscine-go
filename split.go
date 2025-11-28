27161452c94f0cc8d927eb84eda36a105896e51c
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-28 05:08:42 +0300

    Init commit for split.go

---
+++ split.go
@@ -0,0 +1,19 @@
+package piscine
+
+func Split(s, sep string) []string {
+	sepStr := []string{}
+	sR := []rune(s)
+	sepR := []rune(sep)
+
+	sRLen := len(sR)
+	sepLen := len(sepR)
+
+	i := 0
+	end := 0
+
+	for i < sRLen {
+		eOfSep := (i + sepLen) - 1
+	}
+
+	return  sepStr
+}
