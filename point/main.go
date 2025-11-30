45389c56fd5d124666acf40d1c9a47126fc7086e
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-30 12:33:07 +0300

    Remove fmt import

--- point/main.go
+++ point/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"github.com/01-edu/z01"
+)
 
 type point struct {
 	x int
@@ -12,10 +14,45 @@ func setPoint(ptr *point) {
 	ptr.y = 21
 }
 
+func convIntToStr(n int) string {
+	base := 10
+	quot := n
+	mod := 0
+
+	if n < 0 {
+		quot = -n
+	}
+
+	digits := []int{}
+	for ; quot > 0; quot /= base {
+		mod = quot % base
+		digits = append(digits, mod)
+	}
+
+	digitsR := []rune{}
+	for i := len(digits) - 1; i >= 0; i-- {
+		r := rune('0' + digits[i])
+		digitsR = append(digitsR, r)
+	}
+
+	return string(digitsR)
+}
+
 func main() {
 	points := &point{}
 
 	setPoint(points)
 
-	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+	resStr := "x = "
+	xStr := convIntToStr(points.x)
+	resStr += xStr
+
+	resStr += ", y = "
+	yStr := convIntToStr(points.y)
+	resStr += yStr
+
+	for _, r := range resStr {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
 }
