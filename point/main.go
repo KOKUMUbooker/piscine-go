f65545122e6c7fa58217f711e3f933006f987b7c
KOKUMUbooker <bookerochieng20@gmail.com>
2025-11-30 13:13:13 +0300

    Remove all illegal imports

--- point/main.go
+++ point/main.go
@@ -14,45 +14,44 @@ func setPoint(ptr *point) {
 	ptr.y = 21
 }
 
-func convIntToStr(n int) string {
-	base := 10
-	quot := n
-	mod := 0
-
-	if n < 0 {
-		quot = -n
-	}
-
-	digits := []int{}
-	for ; quot > 0; quot /= base {
-		mod = quot % base
-		digits = append(digits, mod)
-	}
-
-	digitsR := []rune{}
-	for i := len(digits) - 1; i >= 0; i-- {
-		r := rune('0' + digits[i])
-		digitsR = append(digitsR, r)
-	}
-
-	return string(digitsR)
-}
-
 func main() {
+	incrementRuneVal := func(n int) rune {
+		r := '0'
+		for i := 1; i <= n; i++ {
+			r++
+		}
+		return r
+	}
+
+	prinIntRunes := func(n int) {
+		base := 10
+		mod := 0
+
+		digits := []int{}
+		for quot := n; quot > 0; quot /= base {
+			mod = quot % base
+			digits = append(digits, mod)
+		}
+
+		for i := len(digits) - 1; i >= 0; i-- {
+			rVal := incrementRuneVal(digits[i])
+			z01.PrintRune(rVal)
+		}
+	}
+
 	points := &point{}
 
 	setPoint(points)
 
-	resStr := "x = "
-	xStr := convIntToStr(points.x)
-	resStr += xStr
-
-	resStr += ", y = "
-	yStr := convIntToStr(points.y)
-	resStr += yStr
-
-	for _, r := range resStr {
+	for _, r := range "x = " {
 		z01.PrintRune(r)
 	}
+	prinIntRunes(points.x)
+
+	for _, r := range ", y = " {
+		z01.PrintRune(r)
+	}
+	prinIntRunes(points.y)
+
 	z01.PrintRune('\n')
 }
