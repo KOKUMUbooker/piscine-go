6bb1b2ff1cb8a68452d1eea91b00e28e279cfac0
KOKUMUbooker <bookerochieng20@gmail.com>
2025-12-01 14:59:32 +0300

    Update issorted

--- issorted.go
+++ issorted.go
@@ -1,31 +1,35 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
-	// f returns 0 if equal, returns -1 otherwise
-	sorted := append([]int{}, a...)
+	// f Returns positive int if 1st > 2nd,returns 0 if equal, returns -1 otherwise
+	if len(a) < 2 {
+		return true
+	}
 
-	n := len(sorted)
-	for i := 0; i < n-1; i++ {
-		for j := 0; j < n-1; j++ {
-			if sorted[j] > sorted[j+1] {
-				sorted[j], sorted[j+1] = sorted[j+1], sorted[j]
-			}
+	asc := true
+	desc := true
+
+	for i := 0; i < len(a)-1; i++ {
+		cmp := f(a[i], a[i+1])
+
+		if cmp > 0 { // Check 1st vs 2nd for ascending order
+			asc = false
+		}
+		if cmp < 0 { // Check 1st vs 2nd for descending order
+			desc = false
 		}
 	}
 
-	for i, n := range a {
-		areEqual := f(n, sorted[i])
-		if areEqual < 0 {
-			return false
-		}
-	}
-
-	return true
+	return asc || desc
 }
 
 func F(a, b int) int {
+	if a > b {
+		return 1
+	}
 	if a == b {
 		return 0
 	}
+
 	return -1
 }
