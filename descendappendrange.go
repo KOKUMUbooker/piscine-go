fbf478865ceeaa89221bd1cc55a2c3bf47099c7e
KOKUMUbooker <bookerochieng20@gmail.com>
2025-12-03 22:19:36 +0300

    Complete descendappendrange

---
+++ descendappendrange.go
@@ -0,0 +1,14 @@
+package piscine
+
+func DescendAppendRange(max, min int) []int {
+	if max <= min {
+		return []int{}
+	}
+
+	res := []int{}
+	for i := max; i > min; i-- {
+		res = append(res, i)
+	}
+
+	return res
+}
