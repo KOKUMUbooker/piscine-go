7b06cec0864a62b600fd9af142b466a319864f64
KOKUMUbooker <bookerochieng20@gmail.com>
2025-12-04 00:18:01 +0300

    Complete comcheck

---
+++ comcheck/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		return
+	}
+
+	args := os.Args[1:]
+	for _, s := range args {
+		if s == "01" || s == "galaxy" || s == "galaxy 01" {
+			fmt.Println("Alert!!!")
+		}
+	}
+}
