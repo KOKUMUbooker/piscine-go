548429a113bcdc4930dcdc1bd9b082fd37449a08
KOKUMUbooker <bookerochieng20@gmail.com>
2025-12-03 15:52:40 +0300

    Update splitString to accept white spaces

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -1,56 +1,30 @@
 package piscine
 
 func ShoppingSummaryCounter(str string) map[string]int {
-	sentence := splitString(str)
+	words := splitString(str)
+	res := make(map[string]int)
 
-	res := map[string]int{}
-
-	for _, word := range sentence {
-		res[word]++
+	for _, w := range words {
+		res[w]++
 	}
 
 	return res
 }
 
 func splitString(s string) []string {
-	i := 0
-
-	sR := []rune(s)
-	sRLen := len(sR)
-
 	res := []string{}
-	for i < sRLen {
-		end := i + 1
-		for !isUpper(sR[end]) {
-			end++
-			if end == sRLen {
-				break
-			}
-		}
+	current := ""
 
-		wordR := sR[i:end]
-		word := removeSpace(string(wordR))
-		res = append(res, word)
-		if end == sRLen {
-			break
-		}
-		i = end
-	}
-
-	return res
-}
-
-func isUpper(r rune) bool {
-	return r >= 'A' && r <= 'Z'
-}
-
-func removeSpace(s string) string {
-	res := ""
 	for _, r := range s {
-		if r != ' ' {
-			res += string(r)
+		if r == ' ' {
+			res = append(res, current)
+			current = ""
+		} else {
+			current += string(r)
 		}
 	}
+
+	res = append(res, current)
 
 	return res
 }
