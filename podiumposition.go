e9de8a6dab3f1b430631216d4879eae69b289ad7
KOKUMUbooker <bookerochieng20@gmail.com>
2025-12-04 02:30:16 +0300

    complete podiumposition

---
+++ podiumposition.go
@@ -0,0 +1,33 @@
+package piscine
+
+func PodiumPosition(podium [][]string) [][]string {
+	n := len(podium)
+
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-1; j++ {
+			a := podium[j][0]
+			b := podium[j+1][0]
+			if isGreater3(a, b) {
+				podium[j], podium[j+1] = []string{b}, []string{a}
+			}
+		}
+	}
+
+	return podium
+}
+
+func isGreater3(s1, s2 string) bool {
+	n := len(s1)
+	greater := false
+	for i := 0; i < n; i++ {
+		if s1[i] > s2[i] {
+			return true
+		}
+	}
+
+	if !greater && len(s1) > len(s2) {
+		return true
+	}
+
+	return greater
+}
