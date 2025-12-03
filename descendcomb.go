94c80fa11e38ff9c769479d5d4a8b00c887b71be
KOKUMUbooker <bookerochieng20@gmail.com>
2025-12-03 10:45:25 +0300

    Complete descendcomb

---
+++ descendcomb.go
@@ -0,0 +1,32 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func DescendComb() {
+	first := true
+
+	for i := 99; i >= 0; i-- {
+		for j := 99; j >= 0; j-- {
+			if i == j {
+				continue
+			}
+
+			if !first {
+				z01.PrintRune(',')
+				z01.PrintRune(' ')
+			}
+			first = false
+
+			print2Digit(i)
+			z01.PrintRune(' ')
+			print2Digit(j)
+		}
+	}
+
+	z01.PrintRune('\n')
+}
+
+func print2Digit(n int) {
+	z01.PrintRune(rune('0' + n/10))
+	z01.PrintRune(rune('0' + n%10))
+}
