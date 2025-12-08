edb19dfc850c946830663495079d95c9aacbaf24
KOKUMUbooker <bookerochieng20@gmail.com>
2025-12-08 17:05:55 +0300

    Func renames

--- listpushfront.go
+++ listpushfront.go
@@ -1,17 +1,17 @@
 package piscine
 
-type NodeL struct {
+type NodeL2 struct {
 	Data interface{}
-	Next *NodeL
+	Next *NodeL2
 }
 
-type List struct {
-	Head *NodeL
-	Tail *NodeL
+type List2 struct {
+	Head *NodeL2
+	Tail *NodeL2
 }
 
-func ListPushFront(l *List, data interface{}) {
-	n := &NodeL{Data: data}
+func ListPushFront(l *List2, data interface{}) {
+	n := &NodeL2{Data: data}
 
 	if l.Head == nil {
 		l.Head = n
