8e4afd5e17289f39d3a9ba042de607c6a17dd53d
KOKUMUbooker <bookerochieng20@gmail.com>
2025-12-08 18:08:19 +0300

    Found better way of clearing list

--- listclear.go
+++ listclear.go
@@ -1,24 +1,19 @@
 package piscine
 
 func ListClear(l *List) {
-	if l == nil || l.Head == nil {
+	if l == nil {
 		return
 	}
 
-	nodes := []*NodeL{}
-	n := l.Head
-	for n.Next != nil {
-		nodes = append(nodes, n)
-		n = n.Next
+	// Walk through the list whilst breaking all links
+	for n := l.Head; n != nil; {
+		next := n.Next
+		n.Data = nil
+		n.Next = nil
+		n = next
 	}
 
-	for i := len(nodes) - 1; i >= 0; i-- {
-		nI := nodes[i]
-		nI.Data = nil
-		nI.Next = nil
-	}
-
+	// Reset the list
 	l.Head = nil
 	l.Tail = nil
-	l = nil
 }
