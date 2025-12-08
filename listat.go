5ace633375ed21c122389f67f3858aca38a46d1b
KOKUMUbooker <bookerochieng20@gmail.com>
2025-12-08 20:02:02 +0300

    complete listat.go

---
+++ listat.go
@@ -0,0 +1,32 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+func ListAt(l *NodeL, pos int) *NodeL {
+	if l == nil {
+		return nil
+	}
+	
+	n := l
+	cur := 0
+	found := false
+
+	for n != nil {
+		if cur == pos {
+			found = true
+			break
+		}
+		
+		cur++
+		n = n.Next
+	}
+
+	if found {
+		return n
+	}
+
+	return nil
+}
