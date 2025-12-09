b853c465aac74b53715a14fed2b269a6b264cb37
KOKUMUbooker <bookerochieng20@gmail.com>
2025-12-09 06:06:46 +0300

    complete listmerge.go

---
+++ listmerge.go
@@ -0,0 +1,28 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListMerge(l1 *List, l2 *List) {
+	if l1 == nil || l2 == nil {
+		return
+	}
+
+	n1 := l1.Tail
+	n2 := l2.Head
+
+	for n2 != nil {
+		n1.Next = n2
+		n1 = n2
+		n2 = n2.Next
+	}
+
+	l1.Tail = l2.Tail
+}
