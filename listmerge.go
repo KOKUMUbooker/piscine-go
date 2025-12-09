c91b34ad4b6a1bba95de669c03f7bbdd534364d5
KOKUMUbooker <bookerochieng20@gmail.com>
2025-12-09 06:58:45 +0300

    Add l2.Head != nil check

--- listmerge.go
+++ listmerge.go
@@ -18,12 +18,14 @@ func ListMerge(l1 *List, l2 *List) {
 	var n1 *NodeL
 	var n2 *NodeL
 
-	if l1.Head == nil && l1.Tail == nil {
+	if l2.Head == nil && l2.Tail == nil {
+		return // l2 empty so just return what l1 had
+	} else if l1.Head == nil && l1.Tail == nil {
 		l1.Head = l2.Head
 		n1 = l2.Head
-		n2 = l2.Head.Next
-	} else if l2.Head == nil && l2.Tail == nil {
-		return // l2 empty so just return what l1 had
+		if l2.Head != nil {
+			n2 = l2.Head.Next
+		}
 	} else {
 		n1 = l1.Tail
 		n2 = l2.Head
