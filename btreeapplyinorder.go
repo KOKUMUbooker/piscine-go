19ca2e6a10d2e4bc7a90a4d8668c9cc5f1fa62ce
KOKUMUbooker <bookerochieng20@gmail.com>
2025-12-09 15:20:54 +0300

    Complete btreeapplyinorder.go

---
+++ btreeapplyinorder.go
@@ -0,0 +1,18 @@
+package piscine
+
+func BTreeApplyInorder(root *TreeNode, f func(...interface{}) (int, error)) {
+	if root == nil {
+		return
+	}
+
+	if root.Left != nil {
+		BTreeApplyInorder(root.Left, f)
+	}
+
+	// Prioritise printing left most nodes before all others
+	f(root.Data)
+
+	if root.Right != nil {
+		BTreeApplyInorder(root.Right, f)
+	}
+}
