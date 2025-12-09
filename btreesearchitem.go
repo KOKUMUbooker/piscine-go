e3f07a0aa1a8ffeffccdeac58004cbed1559f4f0
KOKUMUbooker <bookerochieng20@gmail.com>
2025-12-09 16:21:36 +0300

    Enrure all nodes are traversed until we find a match

--- btreesearchitem.go
+++ btreesearchitem.go
@@ -1,14 +1,20 @@
 package piscine
 
 func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
 	r := root
-	for !(r == nil || r.Data == elem) {
-		if elem > r.Data { // Search right
+	for r != nil {
+		if elem == r.Data {
+			return r
+		} else if elem > r.Data { // Search right
 			r = r.Right
 		} else { // Search left
 			r = r.Left
 		}
 	}
 
-	return r
+	return nil
 }
