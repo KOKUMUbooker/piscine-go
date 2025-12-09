331107c928384e23077ef67b211a3e00a9b50978
KOKUMUbooker <bookerochieng20@gmail.com>
2025-12-09 15:42:32 +0300

    Complete btreeapplypreorder.go

---
+++ btreeapplypreorder.go
@@ -0,0 +1,16 @@
+package piscine
+
+func BTreeApplyPreorder(root *TreeNode, f func(...interface{}) (int, error)) {
+	if root == nil {
+		return
+	}
+
+	f(root.Data)
+	if root.Left != nil {
+		BTreeApplyPreorder(root.Left, f)
+	}
+
+	if root.Right != nil {
+		BTreeApplyPreorder(root.Right, f)
+	}
+}
