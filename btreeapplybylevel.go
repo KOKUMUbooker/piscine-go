c1377c6e43dd0f57b4e0b37ca72cab0e7ea08d31
KOKUMUbooker <bookerochieng20@gmail.com>
2025-12-12 11:44:40 +0300

    Complete btreeapplybylevel.go

---
+++ btreeapplybylevel.go
@@ -0,0 +1,16 @@
+package piscine
+
+func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
+	if root == nil {
+		return
+	}
+
+	f(root.Data)
+	if root.Left != nil {
+		BTreeApplyByLevel(root.Left, f)
+	}
+
+	if root.Right != nil {
+		BTreeApplyByLevel(root.Right, f)
+	}
+}
