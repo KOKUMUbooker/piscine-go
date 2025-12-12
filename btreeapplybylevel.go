3f971fb501fd494e2901f93ae1a2c98858bd4574
KOKUMUbooker <bookerochieng20@gmail.com>
2025-12-12 11:54:01 +0300

    Format code using gofumpt

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -7,32 +7,32 @@ func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
 
 	nodeData := LevelOrder(root)
 
-	for _, nStr := range nodeData{
+	for _, nStr := range nodeData {
 		f(nStr)
 	}
 }
 
 func LevelOrder(root *TreeNode) []string {
-    if root == nil {
-        return nil
-    }
+	if root == nil {
+		return nil
+	}
 
-    queue := []*TreeNode{root}
-    result := []string{}
+	queue := []*TreeNode{root}
+	result := []string{}
 
-    for len(queue) > 0 {
-        node := queue[0]
-        queue = queue[1:]
+	for len(queue) > 0 {
+		node := queue[0]
+		queue = queue[1:]
 
-        result = append(result, node.Data)
+		result = append(result, node.Data)
 
-        if node.Left != nil {
-            queue = append(queue, node.Left)
-        }
-        if node.Right != nil {
-            queue = append(queue, node.Right)
-        }
-    }
+		if node.Left != nil {
+			queue = append(queue, node.Left)
+		}
+		if node.Right != nil {
+			queue = append(queue, node.Right)
+		}
+	}
 
-    return result
+	return result
 }
