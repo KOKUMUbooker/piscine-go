bcd34bc18e95a2e205413cd48efb84f5def5cb3b
KOKUMUbooker <bookerochieng20@gmail.com>
2025-12-12 11:52:55 +0300

    Ensure tree is traversed via level order

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -5,12 +5,34 @@ func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
 		return
 	}
 
-	f(root.Data)
-	if root.Left != nil {
-		BTreeApplyByLevel(root.Left, f)
-	}
+	nodeData := LevelOrder(root)
 
-	if root.Right != nil {
-		BTreeApplyByLevel(root.Right, f)
+	for _, nStr := range nodeData{
+		f(nStr)
 	}
+}
+
+func LevelOrder(root *TreeNode) []string {
+    if root == nil {
+        return nil
+    }
+
+    queue := []*TreeNode{root}
+    result := []string{}
+
+    for len(queue) > 0 {
+        node := queue[0]
+        queue = queue[1:]
+
+        result = append(result, node.Data)
+
+        if node.Left != nil {
+            queue = append(queue, node.Left)
+        }
+        if node.Right != nil {
+            queue = append(queue, node.Right)
+        }
+    }
+
+    return result
 }
