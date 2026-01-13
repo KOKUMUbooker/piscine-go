11926d0ed4e4e02d2c7572486473da9a70171b2c
KOKUMUbooker <bookerochieng20@gmail.com>
2026-01-13 11:01:56 +0300

    Add starter comments

---
+++ go-reloaded/main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	// 1. Split sentence into words with space as separator
+	// 2. Initialize string array for the result
+	// 3. Loop through array of words, looking for mod keys & if found call utility functions on the previous word
+	// 4. Join the result string with space as separator
+}
